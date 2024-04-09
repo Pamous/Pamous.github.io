7ef8ea5dd59eccc88757ba190ce70ee39b4a8e11
Pamous <157498835+Pamous@users.noreply.github.com>
2024-04-09 23:11:30 +0800

    update pull not a framework just a simple html

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pamous.Org is Under Construction of WEB 3.0 Performance</title>
+
+        <!-- STYLE -->
+        <link rel="stylesheet" href="bootstrap/css/display.css">
+        <!-- X-ICON -->
+        <link rel="icon" type="image/x-icon" href="bootstrap/svg/logo/linear_logo_inverse_2.svg">
+    </head>
+    <body>
+        <div class="container d-flex pad-2xl justify-content-center">
+            <div class="wrapper pad-2xl d-flex align-items-center">
+                <!-- IMAGE -->
+                <img src="bootstrap/svg/logo/linear_logo_inverse_2.svg" alt="Pam" title="Meet Pam">
+                <!-- TEXT -->
+                <div class="text-container d-flex justify-content-center align-items-center pad-xl flex-column">
+                    <h3 class="title text-align-center">This Website is under construction for WEB 3.0 Development</h3>
+                    <p class="subtext">Stay Tune on <a class="unique" href="#timestamp">September 2024</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="footer d-flex flex-column align-items-center">
+            <p class="dev">BY SYPIER</p>
+            <p class="corp">&#169;2024 NEXT ERA</p>
+        </div>
+    </body>
+</html>
